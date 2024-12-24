e8620ea3dd6f0f2ff924d3b144b708783fd0d34d
Santosh <santoshsantoshsthastha@gmail.com>
2024-12-24 20:45:53 +0545

    did some of the configurations with the nvim

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -9,13 +9,13 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 local opts = { noremap = true, silent = true }
 
 -- save file
-vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+--vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
 
 -- save file without auto-formatting
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
 
 -- quit file
-vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+--vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
 
 -- delete single character without copying into register
 vim.keymap.set('n', 'x', '"_x', opts)
