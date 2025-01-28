8df62ed71b0dd2969dd34a4f42af6549dbf753ef
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-28 17:11:12 +0545

    did some of the tmuxifier backups

---
+++ tmuxifier/.tmuxifier/layouts/rust_in_progress.session.sh
@@ -0,0 +1,25 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/rustlings/"
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "rust_in_progress"; then
+
+    # Create a new window inline within session layout definition.
+    new_window "Dashboard"
+    run_cmd "rustlings"
+    new_window "exercise"
+    run_cmd "nvim exercises"
+    new_window "solutions"
+    run_cmd "nvim solutions"
+    new_window "terminal"
+    # Load a defined window layout.
+    #load_window "example"
+
+    # Select the default active window on session creation.
+    select_window 2
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
---
+++ tmuxifier/.tmuxifier/layouts/web_in_progress.session.sh
@@ -0,0 +1,24 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/dev/beginner-web/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "web_in_progress"; then
+
+    # Create a new window inline within session layout definition.
+    new_window "code"
+    run_cmd "nvim ."
+    new_window "live_server"
+    run_cmd "live-server"
+    new_window "terminal"
+
+    # Load a defined window layout.
+
+    # Select the default active window on session creation.
+    select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
