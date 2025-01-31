606d154c8fe2aa4f92067fa0d604900ced8a7e8e
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-01-31 18:13:11 +0545

    started the python form some time and lsp configuration is make of python

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -179,23 +179,23 @@ return {
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
       -- ts_ls = {}, -- tsserver is deprecated
-      -- ruff = {}, --it is a linter for python i can un comment it when I want to code in python.
-      -- pylsp = {
-      --   settings = {
-      --     pylsp = {
-      --       plugins = {
-      --         pyflakes = { enabled = false },
-      --         pycodestyle = { enabled = false },
-      --         autopep8 = { enabled = false },
-      --         yapf = { enabled = false },
-      --         mccabe = { enabled = false },
-      --         pylsp_mypy = { enabled = false },
-      --         pylsp_black = { enabled = false },
-      --         pylsp_isort = { enabled = false },
-      --       },
-      --     },
-      --   },
-      -- },
+      ruff = {}, --it is a linter for python i can un comment it when I want to code in python.
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              pyflakes = { enabled = false },
+              pycodestyle = { enabled = false },
+              autopep8 = { enabled = false },
+              yapf = { enabled = false },
+              mccabe = { enabled = false },
+              pylsp_mypy = { enabled = false },
+              pylsp_black = { enabled = false },
+              pylsp_isort = { enabled = false },
+            },
+          },
+        },
+      },
       html = { filetypes = { 'html', 'twig', 'hbs' } },
       cssls = {},
       -- tailwindcss = {},
