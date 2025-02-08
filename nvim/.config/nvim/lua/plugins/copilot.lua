8dc8004c032e5716e47d59d01284ec15b22be4e7
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-08 14:54:53 +0545

    some head ach

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -10,8 +10,8 @@ return {
       hide_during_completion = vim.g.ai_cmp,
       keymap = {
         accept = false, -- handled by nvim-cmp / blink.cmp
-        next = '<M-]>',
-        prev = '<M-[>',
+        next = '<C-n>',
+        prev = '<C-p>',
       },
     },
     panel = { enabled = false },
