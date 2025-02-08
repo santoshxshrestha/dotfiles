ccb800e309f6e8603eb2e9c7c5687502903adf9f
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-08 15:25:41 +0545

    messed again

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -10,7 +10,7 @@ return {
       hide_during_completion = vim.g.ai_cmp,
       keymap = {
         -- accept = false, -- handled by nvim-cmp / blink.cmp
-        accept = '<C-y-y>', -- handled by nvim-cmp / blink.cmp
+        accept = '<C-y>', -- handled by nvim-cmp / blink.cmp
         next = '<C-n>',
         prev = '<C-p>',
       },
