4c6b43564bc358a321e270fcf2c2600171c2cec9
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-02-17 18:30:51 +0545

    some more config in the hyprland .conf and then the session in the tmuxifier
    here I have updated the existing line in the hyprland config file that
    will let me change the sound using the multimedia key and then also set
    the vol to the limit of the 150 percentage and I have commented out the
    previous one that was there for the use of the multimedai keys only.

---
+++ tmuxifier/.tmuxifier/layouts/santosh.session.sh
@@ -0,0 +1,27 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/dev/portfolio/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "santosh"; then
+
+    # Create a new window inline within session layout definition.
+    new_window "code"
+    run_cmd "nvim ."
+
+    new_window "live_server"
+    run_cmd "live-server"
+
+    new_window "terminla"
+
+    # Load a defined window layout.
+    #load_window "example"
+
+    # Select the default active window on session creation.
+    select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
