8ff9b6ffd39180650d725fa062c1030f942d834c
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-05 18:28:12 +0545

    changes

--- nix/nix-shell/web.nix
+++ nix/nix-shell/web.nix
@@ -1,20 +1,20 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
   pkgs = import nixpkgs { 
-    config = {}; 
+    config =  
+
+      { allowUnfree = true; };
     overlays = []; 
   };
 in
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
-    nodejs
-    python3
-    git
-  ];
-  GREETING = "Hello, Nix!";
-  shellHook = ''
+  pkgs.mkShellNoCC {
+    packages = with pkgs; [
+      brave
+    ];
+    GREETING = "Hello, Nix!";
+    shellHook = ''
     echo "Welcome to the development environment!"
     echo $GREETING 
-  '';
-}
+    '';
+  }
 
