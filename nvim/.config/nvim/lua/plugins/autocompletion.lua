51587c255d57b306b8164f5b04f78252ca5bdfbc
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-17 23:22:21 +0545

    the previous one was better

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -128,18 +128,10 @@ return { -- Autocompletion
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         -- Select next/previous item with Tab / Shift + Tab
-        -- ['<Tab>'] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_next_item()
-        --   elseif luasnip.expand_or_locally_jumpable() then
-        --     luasnip.expand_or_jump()
-        --   else
-        --     fallback()
-        --   end
-
-        -- The line upper is the original form the github a edited it according to my need
         ['<Tab>'] = cmp.mapping(function(fallback)
-          if luasnip.expand_or_locally_jumpable() then
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           else
             fallback()
