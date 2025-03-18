4300ecf14c7938087ab8e43356630f60f79e5015
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-18 11:48:01 +0545

    perfect config for the thing here

--- tmuxifier/.tmuxifier/layouts/sync1.session.sh
+++ tmuxifier/.tmuxifier/layouts/sync1.session.sh
@@ -4,33 +4,31 @@ session_root "$HOME/Music/"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "testing"; then
+if initialize_session "sync"; then
 
     # Create a new window inline within session layout definition.
-    new_window "misc"
+    new_window "Haven"
+
     split_h 50
     select_pane 1
-    split_v 50
-    select_pane 3
-    split_v 50
-
-    select_pane 1
-    run_cmd "source ./source/bin/activate"
-    run_cmd "cd ~/Music/Haven/"
+    run_cmd "cd ~/Music/Haven"
+    run_cmd "source ~/Music/Haven/source/bin/activate"
     run_cmd "yt-dlp -x 'https://youtube.com/playlist?list=PLBBcaljFGSiSz8czPRv3tX3C4nKo4On4j&si=YCIbnJX6xgvrvmlV'"
     select_pane 2
-    run_cmd "source ./source/bin/activate"
     run_cmd "cd ~/Music/Eternal-Haven/"
+    run_cmd "source ~/Music/Haven/source/bin/activate"
     run_cmd "yt-dlp -x 'https://youtube.com/playlist?list=PLBBcaljFGSiSZ5enBdlyxcqu0RZtpuuoq&si=xHGIPd-Uw-7DeBZE'"
-    select_pane 3
-    run_cmd "source ./source/bin/activate"
-    run_cmd "cd ~/Music/Instrumental-Haven/"
-    run_cmd "yt-dlp -x 'https://youtube.com/playlist?list=PLBBcaljFGSiQb-vXx89JDd9QXoa_iQjGA&si=yIH54ZTJQtO4vzsF'"
-    select_pane 4
-    run_cmd "source ./source/bin/activate"
-    run_cmd "cd ~/Music/Eternal-Instruemental-Haven/"
-    run_cmd "yt-dlp -x 'https://youtube.com/playlist?list=PLBBcaljFGSiSMYqaq-Vs-1hmnMyCvzW7D&si=8af86RAPfGnFQ5fE'"
 
+    new_window "Insturemental"
+    split_h 50
+    select_pane 1
+    run_cmd "cd ~/Music/Instrumental-Haven"
+    run_cmd "source ~/Music/Haven/source/bin/activate"
+    run_cmd "yt-dlp -x 'https://youtube.com/playlist?list=PLBBcaljFGSiQb-vXx89JDd9QXoa_iQjGA&si=yIH54ZTJQtO4vzsF'"
+    select_pane 2
+    run_cmd "cd ~/Music/Eternal-Instruemental-Haven/"
+    run_cmd "source ~/Music/Haven/source/bin/activate"
+    run_cmd "yt-dlp -x 'https://youtube.com/playlist?list=PLBBcaljFGSiSMYqaq-Vs-1hmnMyCvzW7D&si=8af86RAPfGnFQ5fE'"
     # Load a defined window layout.
     #load_window "example"
 
