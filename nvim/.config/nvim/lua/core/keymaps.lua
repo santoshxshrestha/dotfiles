2c82b43af149a1bcd22e5d0efad35658a9879d51
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-19 14:52:05 +0545

    key map for dismissing noice messages

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -74,3 +74,6 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Dismiss Noice Message
+vim.keymap.set('n', '<leader>q', '<cmd>NoiceDismiss<CR>', { desc = 'Dismiss Noice Message' })
