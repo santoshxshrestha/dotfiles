c82188407a6529a1b0bb663d71675d7d52b96798
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-08 15:57:10 +0545

    added the ts ls

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -201,7 +201,7 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- ts_ls = {}, -- tsserver is deprecated
+      ts_ls = {}, -- tsserver is deprecated
       -- ruff = {}, --it is a linter for python i can un comment it when I want to code in python.
       -- pylsp = {
       --   settings = {
